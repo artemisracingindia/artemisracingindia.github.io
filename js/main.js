e6547b5e464ee2d2074cfc5b2af9877742dbcc5a
artemisracingindia <56833343+artemisracingindia@users.noreply.github.com>
2020-01-01 14:13:08 -0500

    counter

--- js/main.js
+++ js/main.js
@@ -142,20 +142,3 @@ $("#button").click(function(e){
 
 })(jQuery);
 
-
-/*------------------
-        Counter
-    --------------------*/
-
-    var n = localStorage.getItem('on_load_counter');
-
-    if (n === null) {
-        n = 0;
-    }
-
-    n++;
-
-    localStorage.setItem("on_load_counter", n);
-
-    document.getElementById('CounterVisitor').innerHTML = n;
-
